7bfbc671ae3f848b46799c38678180f7d2d92b05
saminthapa <thapasamin4@gmail.com>
2025-02-10 11:42:50 +0545

    ok

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -3,12 +3,20 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="bg-gradient-to-r from-purple-500 to-indigo-500 text-white flex flex-col items-center py-1">
-        <p className="text-center text-lg font-medium mt-4">
-          &copy; 2025 <span className="text-yellow-300">Sagun Gautam</span> All rights reserved. Developed by_ 
-          <a href="https://www.saminthapa.com.np/" className="text-yellow-300 hover:text-white transition duration-300" target="_blank" rel="noopener noreferrer">
-            Samin Thapa
-          </a>
-        </p>
+      <p className="text-center text-lg font-medium mt-4">
+        &copy; 2025 <span className="text-yellow-300">Sagun Gautam</span> All rights reserved.
+      </p>
+      <span className="text-center">
+        Developed by_
+        <a
+          href="https://www.saminthapa.com.np/"
+          className="text-yellow-300 hover:text-white transition duration-300"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Samin Thapa
+        </a>
+      </span>
     </div>
   );
 };
--- app/ImageSlider.jsx
+++ app/ImageSlider.jsx
@@ -1,27 +1,16 @@
 'use client';
-
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import Image from 'next/image';
-import Link from 'next/link';
-
-const images = [
-  '/s1.jpg',
-  '/s2.jpg',
-  '/s3.jpg',
-  '/s4.jpg',
-  '/s5.jpg',
-  '/s6.jpg',
-  '/s7.jpg',
-  '/s8.jpg',
-];
 
 const ImageSlider = () => {
+  const images = ['/s1.jpg', '/s2.jpg', '/s3.jpg', '/s4.jpg', '/s5.jpg', '/s6.jpg', '/s7.jpg', '/s8.jpg'];
   return (
-    <div className="w-[90%] mx-auto h-screen"> {/* Increased width to 90% */}
+    <div className="w-[90%] mx-auto h-screen">
       <Swiper
         modules={[Navigation, Pagination, Autoplay]}
         spaceBetween={20}

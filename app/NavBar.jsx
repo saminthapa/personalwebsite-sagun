d1c7a245afbc3a405b1bc8c467939ce2066c950d
saminthapa <thapasamin4@gmail.com>
2025-02-19 00:15:21 +0545

    logo

--- app/NavBar.jsx
+++ app/NavBar.jsx
@@ -1,13 +1,19 @@
+import Image from 'next/image';
 import React from 'react';
 
 const NavBar = () => {
   return (
-    <header className="fixed inset-x-0 top-6 z-30 mx-auto w-[90%] max-w-screen-lg border border-gray-100 bg-gradient-to-r from-indigo-600 via-purple-500 to-pink-500 py-3 shadow-lg backdrop-blur-lg rounded-3xl">
+    <header className="fixed inset-x-0 top-6 z-30 mx-auto w-[30%] max-w-screen-lg border border-gray-100 bg-gradient-to-r from-indigo-600 via-purple-500 to-pink-500  shadow-lg backdrop-blur-lg rounded-3xl">
       <div className="px-4">
         <div className="flex items-center justify-center">
-          <a className="text-2xl font-bold text-white hover:text-yellow-300 transition duration-300" href="/">
-            Sagun
-          </a>
+          
+            <Image 
+            src='/logo.png'
+             alt='logo'
+             width={70}
+             height={20}/>
+
+        
         </div>
       </div>
     </header>
